54a56bfc193cd5f9baa0f1e299e43ebabd50f613
Yama <souichirou19960210@gmail.comn>
2018-04-16 18:38:23 +0900

    create controller tasks and action

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,37 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
+  def inddx
+    @tasks = Task.all
+  end
+  
+  def show
+  end
+  
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = Task.new(task_params)
+  end
+  
+  def edit
+  end
+  
+  def update
+  
+  end
+  
+  def destroy
+  end
+  
+  private
+    def task_params
+      params.require(:task).permit(:title, :description, :dead, :priority)
+    end
+    
+    def set_task
+      @task = Task.find(params[:id])
+    end
+end
