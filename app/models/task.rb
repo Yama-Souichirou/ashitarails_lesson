dcd0150990265764550b425e712e5067cc172ff0
Yama <souichirou19960210@gmail.comn>
2018-04-20 12:00:24 +0900

    task.rb ステータスと順位をenumか

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,8 @@
 class Task < ApplicationRecord
+  PRIORITIES = { "最優先": 1, "優先": 2, "普通": 3, "お手すき" }
+  STATUSES   = { "完了": 1, "着手中": 2, "未着手": 3, "お手すき" }
+  
+  enum priorities: PRIORITIES
+  enum statuses: STATUSES
+  
 end
