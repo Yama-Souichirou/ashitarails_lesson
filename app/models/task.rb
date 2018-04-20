40ba9e59f25f458eea9437e57ec5ac29c1d8a0bf
Yama <souichirou19960210@gmail.comn>
2018-04-20 15:06:29 +0900

    save

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,8 @@
 class Task < ApplicationRecord
-  PRIORITIES = { 最優先: 1, 優先: 2, 普通: 3, お手すき: 4 }
-  STATUSES   = { 完了: 1, 着手中: 2, 未着手: 3 }
+  PRIORITIES = { "最優先" => 1, "優先" => 2, "普通" => 3, "お手すき" => 4 }
+  STATUSES   = { "完了" => 1, "着手中" => 2, "未着手" => 3 }
   
   enum priority: PRIORITIES
   enum status: STATUSES
-  
 end
+
