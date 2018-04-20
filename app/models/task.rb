c4640089a1fd7f2bd5cf3aa44330f6c050c43642
Yama <souichirou19960210@gmail.comn>
2018-04-20 12:03:54 +0900

    enumの記述が間違っていたので修正

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
-  PRIORITIES = { "最優先": 1, "優先": 2, "普通": 3, "お手すき" }
-  STATUSES   = { "完了": 1, "着手中": 2, "未着手": 3, "お手すき" }
+  PRIORITIES = { "最優先": 1, "優先": 2, "普通": 3, "お手すき": 4 }
+  STATUSES   = { "完了": 1, "着手中": 2, "未着手": 3 }
   
   enum priorities: PRIORITIES
   enum statuses: STATUSES
