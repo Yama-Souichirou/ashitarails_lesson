4e634e0d5f56b9e3c6fcf794844433f754d90ebb
Yama <souichirou19960210@gmail.comn>
2018-04-20 11:48:56 +0900

    tasks_controler 7action save

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,39 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.all end
+    @tasks = Task.all
+  end
+  
+  def show
+  end
+  
+  def new
+    @task = Task.new
+  end
+  
+  def create
+  
+  end
+  
+  def edit
+  
+  end
+  
+  def update
+  
+  end
+  
+  def destroy
+  
+  end
+  
+  private
+    def task_params
+      params.require(:task).permit(:title, :description, :deadline_on, :priority, :status, :user_id)
+    end
+  
+    def set_params
+      @task = Task.find(params[:id])
+    end
 end
