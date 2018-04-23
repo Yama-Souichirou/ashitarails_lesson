cae95bb5704e1628e5453a0cea8ecf60801dcf21
Yama <souichirou19960210@gmail.comn>
2018-04-23 14:35:59 +0900

    feature spec リファクタ

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -16,29 +16,33 @@ RSpec.feature "Tasks", type: :feature do
     end
   end
   
-  describe "sorted" do
-    it "created_at" do
-      tasks = []
-      1.upto(3) do |i|
-        tasks << FactoryGirl.create(:task, created_at: Date.today + i)
+  describe "sort" do
+    context "default" do
+      it "ordered created_at" do
+        tasks = []
+        1.upto(3) do |i|
+          tasks << FactoryGirl.create(:task, created_at: Date.today + i)
+        end
+        visit root_path
+
+        expect(page.all("tbody tr")[0].find(".td-title").text).to match "タスク名3"
+        expect(page.all("tbody tr")[1].find(".td-title").text).to match "タスク名2"
+        expect(page.all("tbody tr")[2].find(".td-title").text).to match "タスク名1"
       end
-      visit root_path
-  
-      expect(page.all("tbody tr")[0].find(".td-title").text).to match "タスク名3"
-      expect(page.all("tbody tr")[1].find(".td-title").text).to match "タスク名2"
-      expect(page.all("tbody tr")[2].find(".td-title").text).to match "タスク名1"
     end
     
     context "check '期日' and click button '適応'" do
-      it "ordred deadline_on" do
+      before do
+        visit root_path
+        check "deadline_on"
+        click_button "適応"
+      end
+      
+      it "ordered deadline_on" do
         tasks = []
         1.upto(3) do |i|
           tasks << FactoryGirl.create(:task, deadline_on: Date.today + i)
         end
-
-        visit root_path
-        check "deadline_on"
-        click_button "適応"
 
         expect(page.all("tbody tr")[0].find(".td-deadlineon").text).to match "#{(Date.today + 3).strftime("%Y/%m/%d")}"
         expect(page.all("tbody tr")[1].find(".td-deadlineon").text).to match "#{(Date.today + 2).strftime("%Y/%m/%d")}"
