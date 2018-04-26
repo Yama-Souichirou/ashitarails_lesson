c3d56bab9aec344acfe71bf587ba230bedce9529
Yama <souichirou19960210@gmail.comn>
2018-04-26 10:33:43 +0900

    add column remember_token to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180424010241) do
+ActiveRecord::Schema.define(version: 20180426013238) do
 
   create_table "labels", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20180424010241) do
     t.string   "username"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "remember_token"
   end
 
   add_foreign_key "task_labels", "labels"
