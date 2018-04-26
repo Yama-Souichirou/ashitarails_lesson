9b30e6ec553a20c3cbf0f0b41b4f33949190febd
Yama <souichirou19960210@gmail.comn>
2018-04-26 12:39:25 +0900

    ログアウト機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tasks, except: :show
   resources :registrations
-  resources :sessions
+  resources :sessions, only: [:new, :create]
+  delete "/session" => "sessions#destroy", as: :destroy_session
   
   root "tasks#index"
 end
