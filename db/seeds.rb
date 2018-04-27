5e20523c842a8350f3eb910e8c15902b24c7b37b
Yama <souichirou19960210@gmail.comn>
2018-04-27 09:55:12 +0900

    ラベルシード追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create(username: "yamasou", email: "s.yama@ashita-team.com", password: "password", password_confirmation: "password")
 User.create(username: "山口", email: "guti@guti", password: "password", password_confirmation: "password")
+
+label_names = ["業務報告", "経費計算", "資料作成", "その他"]
+label_names.each do |name|
+  Label.create(name: name)
+end
