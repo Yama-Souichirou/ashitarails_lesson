3c40d330b5dc5f18fb83cd8a6b835b98bdae8900
Yama <souichirou19960210@gmail.comn>
2018-05-07 15:05:59 +0900

    add dependent to user and label

--- app/models/label.rb
+++ app/models/label.rb
@@ -1,5 +1,5 @@
 class Label < ApplicationRecord
-  has_many :task_labels
+  has_many :task_labels, dependent: :delete_all
   has_many :tasks, through: :task_labels
   
   validates :name, presence: true
