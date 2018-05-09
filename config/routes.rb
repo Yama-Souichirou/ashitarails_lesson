06ecfce2b31e37a3f38c4744e07ab064859a47e7
Yama <souichirou19960210@gmail.comn>
2018-05-09 16:41:32 +0900

    admin root修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,4 @@
-class AuthenticatedConstraint
-  def matches?(request)
-    user =  User.find_by(remember_token: Digest::SHA256.hexdigest(request.cookies["user_remember_token"].to_s))
-    user.present? && user.admin?
-  end
-end
-
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  constraints AuthenticatedConstraint.new do
-    root to: 'admin/users#index'
-  end
   resources :tasks, except: :show
   resources :sessions, only: [:new, :create]
   resources :labels, only: [:index]
@@ -17,6 +6,7 @@ Rails.application.routes.draw do
   delete "/session" => "sessions#destroy", as: :destroy_session
   
   namespace :admin do
+    root 'users#index'
     resources :users, only: [:index, :show]
     resources :tasks, except: :show
     resources :registrations
