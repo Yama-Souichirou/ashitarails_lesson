10306ef295aff3ca99a7ba90f25f2b8c3a420b00
Yama <souichirou19960210@gmail.comn>
2018-05-17 14:29:04 +0900

    models テスト models/group_spec.rb models/group_user_spec.rb作成

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a title, deadline_on, status, and priority" do
+    group = FactoryGirl.build(:group)
+    expect(group).to be_valid
+  end
+  
+  it "is invalid without name" do
+    group = FactoryGirl.build(:group, name: nil)
+    group.valid?
+    expect(group.errors[:name]).to include("を入力してください")
+  end
 end
--- spec/models/group_user_spec.rb
+++ spec/models/group_user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe GroupUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:group) { FactoryGirl.create(:group) }
+  let(:user) { FactoryGirl.create(:user) }
+  it "is valid with a group, user" do
+    group_user = FactoryGirl.build(:group_user, user: user, group: group)
+    expect(group_user).to be_valid
+  end
+  
 end
